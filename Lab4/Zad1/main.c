2e68a19677c1b96cbd2b83d0edbd66c6821f19e2
hardkov <krzysztofh150@interia.pl>
2020-03-29 00:47:43 +0100

    Lab4 Zad1 done

---
+++ Lab4/Zad1/main.c
@@ -0,0 +1,38 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int PROGRAM_STATE; // -1 paused 1 unpaused
+
+void int_handler(int signum){
+    printf("\nOdebrano sygnał SIGINT\n");
+    exit(0);
+}
+
+void stp_handler(int sig_no){
+    printf("\nOczekuję na CTRL+Z - kontynuacja albo CTR+C - zakończenie programu\n");
+    if(PROGRAM_STATE == -1) PROGRAM_STATE = 1;
+    else PROGRAM_STATE = -1;
+}
+
+int main(int argc, char* argv[]){
+    PROGRAM_STATE = 1;
+    printf("%d\n", getpid());
+    
+    struct sigaction act;
+    act.sa_handler = stp_handler;
+    sigemptyset(&act.sa_mask);  
+    act.sa_flags = 0;
+    
+    sigaction(SIGTSTP, &act, NULL);
+    signal(SIGINT, int_handler);
+
+    while(1){
+        system("ls");
+        sleep(1);
+        if(PROGRAM_STATE == -1) pause();
+    };
+
+    return 0;
+}
