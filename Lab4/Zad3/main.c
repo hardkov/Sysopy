8389cef9a402a64526db2d21d345d4f68ed832d9
hardkov <krzysztofH150@interia.pl>
2020-03-29 21:44:15 +0200

    Lab4 Zad3 done

--- Lab4/Zad3/main.c
+++ Lab4/Zad3/main.c
@@ -3,32 +3,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <wait.h>
 
-void handler(int sig, siginfo_t* info, void* ucontext){
-    printf("PID: %d SIG: %d UID: %d CODE: %d SYS TIME: %ld\n", info -> si_pid, sig, info -> si_uid,
-    info -> si_code, info -> si_stime);   
+void sgf_handler(int sig, siginfo_t* info, void* ucontext){
+    printf("SIGSEGV captured:\nProblematic memory location:%p\n\n", info -> si_addr);
+    printf("SIGSEGV was provoked by accessing a restricted area of memory\n");   
+    exit(0);
 }
 
-int main(int argc, char* argv[]){
-    int i;
+void chld_handler(int sig, siginfo_t* info, void* ucontext){
+    printf("SIGCHLD captured:\nUser id: %d\nExit value: %d\nUser time: %ld\nSystem time: %ld\n\n", info -> si_uid, info -> si_status,
+    info -> si_utime, info -> si_stime);
+    printf("SIGCHLD was provoked by creating child process and ending it\n");
+    exit(0);
+}
 
-    if(argc < 2) printf("usage: ./main <list of signal identificators(numbers), space-separated>\n");
+void fpe_handler(int sig, siginfo_t* info, void* ucontext){
+    printf("SIGFPE captured:\nProblematic memory location:%p\n\n", info -> si_addr);
+    printf("SIGFPE was provoked by dividing by zero\n");
+    exit(0);
+}
+int main(int argc, char* argv[]){
+    if(argc < 2){
+        printf("usage: ./main sig\n");
+        exit(0);
+    }
     
+    int sig;
+
+    sig = atoi(argv[1]);
+
     struct sigaction act;
     act.sa_flags = SA_SIGINFO;
     sigemptyset(&act.sa_mask);
-    act.sa_sigaction = handler;
+
+    act.sa_sigaction = sgf_handler;
+    sigaction(SIGSEGV, &act, NULL);  // installing signal handlers
     
-    printf("%d\n", getpid());
+    act.sa_sigaction = chld_handler;
+    sigaction(SIGCHLD, &act, NULL); 
 
-    for(i = 1; i < argc; i++){
-        sigaction(atoi(argv[i]), &act, NULL);  // installing signal handlers
-    }
-    while(1){
-            printf("Working...\n");
-            sleep(1);    
-    }
+    act.sa_sigaction = fpe_handler;
+    sigaction(SIGFPE, &act, NULL);  
 
+    if(sig == 11){
+        char *s = "test string";  // provoke segfault
+        *s = 'A';
+    }
+    else if(sig == 17){
+        if(fork() == 0){
+            while(sig < 2000000000) sig++;           // make babies
+            return 1;
+        }
+        wait(NULL);
+    }
+    else if(sig == 8){
+        sig = 0;
+        sig = 1/sig;     // divide by 0
+    }
+    
     return 0;
 }
 
